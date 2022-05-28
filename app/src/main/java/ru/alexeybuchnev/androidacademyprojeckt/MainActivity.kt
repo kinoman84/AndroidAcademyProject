48d7c70a428881d36a720d511345a9ed91361d72
kinoman <a.buchnev.kinoman@gmail.com>
2022-05-28 16:01:42 +0300

    refactor activity_movie_details.xml to fragment add film list fragment

--- app/src/main/java/ru/alexeybuchnev/androidacademyprojeckt/MainActivity.kt
+++ app/src/main/java/ru/alexeybuchnev/androidacademyprojeckt/MainActivity.kt
@@ -3,9 +3,28 @@ package ru.alexeybuchnev.androidacademyprojeckt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesList.Callbacks {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_main)
+
+        //TODO если переменную понадобится вынести, нужно будет её проинициализировать после переворота экрана
+        if (savedInstanceState == null) {
+            val fragmentMovieList = FragmentMoviesList.newInstance("param1", "param2")
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragmentMovieList)
+                .commit()
+        }
+    }
+
+    override fun onFilmSelectedClick(filmName: String) {
+        val fragmentMoviesDetails = FragmentMoviesDetails.newInstance("","")
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, fragmentMoviesDetails)
+            .addToBackStack(null)
+            .commit()
     }
 }
