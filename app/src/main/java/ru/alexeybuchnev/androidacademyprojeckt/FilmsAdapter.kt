9c59a4a0c98c992363d0dd4bbbee4b23159bee59
kinoman <a.buchnev.kinoman@gmail.com>
2022-06-10 21:05:26 +0300

    add glide

--- app/src/main/java/ru/alexeybuchnev/androidacademyprojeckt/FilmsAdapter.kt
+++ app/src/main/java/ru/alexeybuchnev/androidacademyprojeckt/FilmsAdapter.kt
@@ -6,9 +6,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ru.alexeybuchnev.androidacademyprojeckt.model.Movie
 
-class FilmsAdapter(val clickListener: FragmentMoviesList.Callbacks?) : RecyclerView.Adapter<FilmListItemViewHolder>() {
+class FilmsAdapter(val clickListener: FragmentMoviesList.Callbacks?) : RecyclerView.Adapter<FilmsAdapter.FilmListItemViewHolder>() {
     private var filmsList = listOf<Movie>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmListItemViewHolder {
         val view: View =
@@ -17,11 +19,11 @@ class FilmsAdapter(val clickListener: FragmentMoviesList.Callbacks?) : RecyclerV
     }
 
     override fun onBindViewHolder(holder: FilmListItemViewHolder, position: Int) {
+        holder.bindFilm(filmsList[position])
+
         holder.itemView.setOnClickListener {_ ->
             clickListener?.onFilmSelectedClick(position)
         }
-
-        holder.bindFilm(filmsList[position])
     }
 
     override fun getItemCount(): Int {
@@ -31,19 +33,35 @@ class FilmsAdapter(val clickListener: FragmentMoviesList.Callbacks?) : RecyclerV
     fun bindMovies(moviesList: List<Movie>) {
         this.filmsList = moviesList
     }
-}
 
-class FilmListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class FilmListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private lateinit var movie: Movie
+        private lateinit var movie: Movie
 
-    private val filmNameTextView: TextView = itemView.findViewById(R.id.movie_name_text)
-    private var filmPosterImageView: ImageView = itemView.findViewById(R.id.movie_logo_image)
+        private val filmNameTextView: TextView = itemView.findViewById(R.id.filmNameTextView)
+        var filmPosterImageView: ImageView = itemView.findViewById(R.id.film_poster_image_view)
 
-    fun bindFilm(film: Movie) {
-        this.movie = film
-        filmNameTextView.text = film.title
-        //filmPosterImageView.setImageResource(film.imageResourceId)
+
+        fun bindFilm(film: Movie) {
+            this.movie = film
+            filmNameTextView.text = film.title
+            //Glide.with(itemView.context).load(movie.imageUrl).into(filmPosterImageView)
+            Glide.with(itemView.context)
+                //.load("https://image.ibb.co/j142xJ/Amanda_Seyfried.jpg")
+                .load(movie.imageUrl)
+                .apply(imageOption)
+                .into(filmPosterImageView)
+            //filmPosterImageView.setImageResource(R.drawable.film_poster_avenges_end_game)
+            //Log.d("bindFilm", movie.imageUrl)
+        }
+
+        companion object {
+            private val imageOption = RequestOptions()
+                .placeholder(R.drawable.film_poster_avenges_end_game)
+                .fallback(R.drawable.film_poster_avenges_end_game)
+                .centerCrop()
+        }
+
     }
-
 }
+
