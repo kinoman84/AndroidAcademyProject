31727b05c327995d015d7b72ba8614b15b12e69d
kinoman <a.buchnev.kinoman@gmail.com>
2022-06-13 14:35:32 +0300

    add rating on movies list

--- app/src/main/java/ru/alexeybuchnev/androidacademyprojeckt/FilmsAdapter.kt
+++ app/src/main/java/ru/alexeybuchnev/androidacademyprojeckt/FilmsAdapter.kt
@@ -47,9 +47,17 @@ class FilmsAdapter(val clickListener: FragmentMoviesList.Callbacks?) :
         private val reviewersCountTextView: TextView =
             itemView.findViewById(R.id.reviewers_count_text_view)
         private val genresTextView: TextView = itemView.findViewById(R.id.genres_text_view)
-        private val runningTimeTextView: TextView = itemView.findViewById(R.id.running_time_text_view)
-        private val ageRestrictionsTextView: TextView = itemView.findViewById(R.id.age_restrictions_text_view)
+        private val runningTimeTextView: TextView =
+            itemView.findViewById(R.id.running_time_text_view)
+        private val ageRestrictionsTextView: TextView =
+            itemView.findViewById(R.id.age_restrictions_text_view)
 
+
+        private val star1: View = itemView.findViewById(R.id.star1)
+        private val star2: View = itemView.findViewById(R.id.star2)
+        private val star3: View = itemView.findViewById(R.id.star3)
+        private val star4: View = itemView.findViewById(R.id.star4)
+        private val star5: View = itemView.findViewById(R.id.star5)
 
         fun bindFilm(film: Movie) {
             this.movie = film
@@ -77,13 +85,38 @@ class FilmsAdapter(val clickListener: FragmentMoviesList.Callbacks?) :
                 .load(movie.imageUrl)
                 .apply(imageOption)
                 .into(filmPosterImageView)
+
+            star1.background = if (movie.rating >= 1) context.resources.getDrawable(
+                R.drawable.star_icon_on,
+                null
+            ) else context.resources.getDrawable(R.drawable.star_icon_off, null)
+
+            star2.background = if (movie.rating >= 2) context.resources.getDrawable(
+                R.drawable.star_icon_on,
+                null
+            ) else context.resources.getDrawable(R.drawable.star_icon_off, null)
+
+            star3.background = if (movie.rating >= 3) context.resources.getDrawable(
+                R.drawable.star_icon_on,
+                null
+            ) else context.resources.getDrawable(R.drawable.star_icon_off, null)
+
+            star4.background = if (movie.rating >= 4) context.resources.getDrawable(
+                R.drawable.star_icon_on,
+                null
+            ) else context.resources.getDrawable(R.drawable.star_icon_off, null)
+
+            star5.background = if (movie.rating >= 5) context.resources.getDrawable(
+                R.drawable.star_icon_on,
+                null
+            ) else context.resources.getDrawable(R.drawable.star_icon_off, null)
         }
 
         private fun getGenresString(): String {
             var genresString: String = ""
 
             for (genre in movie.genres) {
-                if (genresString == ""){
+                if (genresString == "") {
                     genresString = genresString.plus(genre.name)
                 } else genresString = genresString.plus(", ").plus(genre.name)
             }
